5d1a6763c4cc39318708c6b4f81ea3d11d6360a2
Meet Jivani <meet@Meets-MacBook-Air.local>
2022-12-24 14:55:33 +0530

    Login and Pagination added

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const path = require('path')
 const express = require("express");
 const mongoose = require("mongoose");
 const CrudCollection = require("./crud")
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Helooo");
 })
 
-app.post("/mens", async (req,res)=>{
+app.post("/mens", async (req, res) => {
     try {
         const addUser = new CrudCollection(req.body)
         console.log(req.body);
@@ -23,7 +24,7 @@ app.post("/mens", async (req,res)=>{
     }
 })
 
-app.get("/mens", async (req,res)=>{
+app.get("/mens", async (req, res) => {
     try {
         const result = await CrudCollection.find({});
         res.status(201).send(result)
@@ -32,22 +33,52 @@ app.get("/mens", async (req,res)=>{
     }
 })
 
-app.get("/mens/:id", async (req,res)=>{
+app.get("/mens/:name", async (req, res) => {
     try {
-        const _id = req.params.id;
-        // console.log(_id);
-        const result = await CrudCollection.findById(_id);
+        const name = req.params.name;
+        const result = await CrudCollection.find({name:name});
         res.status(201).send(result)
     } catch (error) {
         res.status(400).send(error);
     }
 })
 
-app.patch("/mens/:id", async (req,res)=>{
+// app.get("/mens/:id", async (req, res) => {
+//     try {
+//         const _id = req.params.id;
+//         // console.log(_id);
+//         const result = await CrudCollection.findById(_id);
+//         res.status(201).send(result)
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// })
+
+app.get("/sort", async (req, res) => {
+    try {
+        const result = await CrudCollection.find({}).sort({ name: 1 });
+        res.status(201).send(result)
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+app.get("/mens/:perpage/:page", async (req, res) => {
+    try {
+        const perpage = Math.max(0, req.params.perpage);
+        const page = Math.max(0, req.params.page) - 1;
+        const result = await CrudCollection.find({}).limit(perpage).skip(perpage * page);
+        res.status(400).send(result);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+app.patch("/mens/:id", async (req, res) => {
     try {
         const _id = req.params.id;
-        const result = await CrudCollection.findByIdAndUpdate(_id,req.body,{
-            new : true
+        const result = await CrudCollection.findByIdAndUpdate(_id, req.body, {
+            new: true
         })
         res.status(201).send(result)
     } catch (error) {
@@ -55,7 +86,7 @@ app.patch("/mens/:id", async (req,res)=>{
     }
 })
 
-app.delete("/mens/:id", async (req,res)=>{
+app.delete("/mens/:id", async (req, res) => {
     try {
         const _id = req.params.id;
         const result = await CrudCollection.findByIdAndDelete(_id)
@@ -65,13 +96,30 @@ app.delete("/mens/:id", async (req,res)=>{
     }
 })
 
-app.listen(port, (req,res)=>{
+app.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const result = await CrudCollection.findOne({ email: email });
+        const match = await bcrypt.compare(password, result.password);
+        if (match) {
+            res.send("login successful")
+        }
+        else {
+            res.status(404).send("Invalid password")
+        }
+    } catch (error) {
+        res.status(404).send("Invalid mail")
+    }
+})
+
+app.listen(port, (req, res) => {
     console.log(`Connection is established on port no. ${port}`);
 })
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/data").then(()=>{
+mongoose.connect("mongodb://localhost:27017/data").then(() => {
     console.log("Connection success");
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error);
 })
